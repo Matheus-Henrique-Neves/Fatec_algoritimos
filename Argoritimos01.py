f1c73e09fdfb66f8400486e6cfdd0920d11a9b95
matheus henrique almeida vieira neves <matheus16092004@gmail.com>
2023-08-22 10:12:17 -0300

    ok com o meu nome

--- Argoritimos01.py
+++ Argoritimos01.py
@@ -1,4 +1,4 @@
-#print("Ola Alunos de redes s2")
+
 print("Escreva a nota um");
 nota1=int(input());
 print("Escreva a nota dois");
@@ -9,7 +9,8 @@ print("Escreva o numero de aulas totais");
 aulastotais=int(input());
 porcentagem_participacao=faltas/aulastotais;
 if (6<=((nota1+nota2)/2)<=10) and (porcentagem_participacao<0.25):
-    print("passou");
+    print("passouu super idol the shogun");
+
 
 else:
     print("reprovou");
